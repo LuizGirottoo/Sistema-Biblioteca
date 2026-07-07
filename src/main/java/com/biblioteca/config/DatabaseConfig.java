5eaff91806525859489f86a20d2e3973267c70ea
Antonio J Santos <antoniosantos.x@hotmail.com>
2026-07-06 21:20:12 -0300

    Update DatabaseConfig.java
    Removed commented test import statement.

--- src/main/java/com/biblioteca/config/DatabaseConfig.java
+++ src/main/java/com/biblioteca/config/DatabaseConfig.java
@@ -7,7 +7,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-//teste importação
 
 /**
  * Fabrica centralizada de conexoes JDBC com o banco SQLite.
